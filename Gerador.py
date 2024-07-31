6c943e846cc476db0a367540d8acfb94db61ab6c
Guilherme <guilhermegutofer@gmail.com>
2024-07-30 23:39:01 -0300

    Se aplicado, esse commit irá adicionar o codigo para que seja possível ver os atendimentos realizados separados por dias, juntamente com quantos e de qual tipo por dia e a somatória do valor arrecadado por dia

--- Gerador.py
+++ Gerador.py
@@ -1,20 +1,39 @@
 import pandas as pd
 
-
-
-# Função para processar os dados
 def processar_atendimentos(file_path):
-    # Ler o arquivo Excel
+
     df = pd.read_excel(file_path)
 
-    # Exibir os dados lidos (para verificação)
+    df_desejado = df[colunas_desejadas]
+
+
+    if 'Cliente' in df_desejado.columns:
+        df_desejado.loc[:, 'Cliente'] = df_desejado['Cliente'].str.replace(r'^\d+-', '', regex=True).str.strip()
+
+    if 'Serviço' in df_desejado.columns:
+        df_desejado.loc[:, 'Serviço'] = df_desejado['Serviço'].str.replace(r'^\d+-', '', regex=True).str.strip()
+
     print("Dados lidos:")
 
-    teste = df.sort_values(by=['Data', 'Valor Estab.'])
+    agrupado = df_desejado.groupby('Data')
 
-    print(teste)
+    for data, group in agrupado:
+        print(f"\nAtendimentos de {data}:")
+        print(group.drop(columns=['Data']).sort_values(by=['Parte', 'Serviço']).to_string(index=False))
+
+        contagem_servicos = group['Serviço'].value_counts().reset_index()
+        contagem_servicos.columns = ['Serviço', 'Quantidade']
 
 
+        print("\nContagem de serviços:")
+        print(contagem_servicos.to_string(index=False))
+
+        x = group['Parte'].sum()
+        print("Ganhos do dia: "+str(x))
+        print("\n----------------------------------------------------\n")
+
+
+colunas_desejadas = ['Data', 'Cliente', 'Serviço', 'Parte']
 
 
 file_path = "atendimentos.xlsx"
